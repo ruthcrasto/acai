dfec1cc520ba81586a097c57d56ab821cdb5fbd0
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2021-04-24 22:49:31 -0400

    lines results

--- runs/vaain.sh
+++ runs/vaain.sh
@@ -1,16 +1,20 @@
 ## A list of all the runs from our experiments ##
 
-# TODO: add different datasets and latent dimensions
-
 # Vanilla VAE and beta-VAE experiments
 vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=1
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=5
 vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=10
-vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=100
-vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=250
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=15
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0 --beta=20
+
 
 # Adversarial VAE
 vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=1
-vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=100
-vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --advweight=0.8 --beta=100
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=5
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=10
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=15
+vaain.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN --beta=20
 
 
+# Adversarial Autoencoder (baseline)
+acai.py --dataset=lines32 --latent_width=2 --depth=16 --latent=16 --train_dir=TRAIN
